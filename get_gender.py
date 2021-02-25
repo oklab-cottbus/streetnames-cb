6a81af0aa048104ef9f3d1bf0d8cc4157266ddef
Sebastian Scholz <scholzsebastian@posteo.de>
2021-02-25 19:38:37 +0100

    Working prototype

--- get_gender.py
+++ get_gender.py
@@ -8,10 +8,16 @@ name = "Douglas Adams"
 
 result = requests.get(base_url, params={"action": "wbsearchentities", "search": name, "language": "de","format":"json"})
 
-id = "Q42"
-
+id = result.json()['search'][0]['id']
 
 result = requests.get(base_url, params={"action": "wbgetentities", "ids":id ,"language":"de","format":"json"})
-gender_id = "Q6581097"
+
+gender_id = result.json()['entities'][id]['claims']['P21'][0]['mainsnak']['datavalue']['value']['id']
 
 result = requests.get(base_url, params={"action": "wbsearchentities", "search":gender_id, "language":"de","format":"json"})
+
+gender = result.json()['search'][0]['label']
+
+print(gender)
+
+
