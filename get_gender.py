28c244f7164030b6e2c09d9bf69c21c6d734d20c
Sebastian Scholz <scholzsebastian@posteo.de>
2021-02-25 19:22:13 +0100

    Update

--- get_gender.py
+++ get_gender.py
@@ -8,3 +8,10 @@ name = "Douglas Adams"
 
 result = requests.get(base_url, params={"action": "wbsearchentities", "search": name, "language": "de","format":"json"})
 
+id = "Q42"
+
+
+result = request.get(base_url, params={"action": "wbgetentities", "ids":id "language":"de","format":"json"})
+gender_id = "Q6581097"
+
+result = request.get(base_url, params={"action": "wbsearchentities", "search":gender_id "language":"de","format":"json"})
