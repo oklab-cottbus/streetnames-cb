716ad404d4dadda7c52f16b5bf83f787390bdfff
Martin Hilbig <martin@mhilbig.de>
2021-06-24 17:54:22 +0200

    fixed `osm_cb.R` (renamed to `names-cottbus.R`), added `names-cottbus.csv` which it creates, closes #6

--- names-cottbus.R
+++ names-cottbus.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(osmdata)
 library(sf)
+library(gdalUtils)
+library(rgdal)
+library(maptools)
+library(raster)
 
 cbs<-getbb("Cottbus Germany")
 
@@ -15,6 +19,16 @@ streets <- getbb("Cottbus Germany") %>%
                             "service", "footway")) %>%
   osmdata_sf()
 
+dsn <- "WFS:https://cardo.cottbus.de/net3/public/ogc.ashx?NodeId=75&Service=WFS&Request=GetCapabilities&"
+ogrinfo(dsn, so=TRUE)
+ogr2ogr(dsn, "L60.shp", "L60")
+
+cb <- readOGR("L60.shp", "L60", stringsAsFactors=FALSE)
+#plot(cb)
+
+#writeSpatialShape(cb, "cb", proj4string=crs(cb))
+#cb_shp <- readOGR("cb.shp", stringsAsFactors=FALSE)
+
 #### crs transform
 cb_4326 <-st_as_sf(cb) %>% st_transform(., crs = 4326)
 
@@ -23,13 +37,15 @@ s_per_q<-lapply(cb_4326$geometry, function(x) {streets$osm_lines[x,] })
 names(s_per_q)<-cb_4326$name
 
 #### controll-plot
-plot(cb_4326$geometry)
-sapply(s_per_q, function(x){
-  Sys.sleep(1)
-  plot(x, add=T)
-})
+#plot(cb_4326$geometry)
+#sapply(s_per_q, function(x){
+#  Sys.sleep(1)
+#  plot(x, add=T)
+#})
 
 #### namelist
 namelist<-sapply(s_per_q, function(x) 
   x$name[!is.na(x$name)] %>% unique(.) 
 )
+
+lapply(namelist, write, "names-cottbus.csv", append=TRUE, ncolumns=1)
